e02ae67f0ab675f149a16e468247319e7aed6f9c
tsang gary <ws_wishao@163.com>
2013-03-14 22:17:36 +0800

    test ibatis and spring

---
+++ src/main/java/com/reader/core/impl/TestDaoImpl.java
@@ -0,0 +1,33 @@
+package com.reader.core.impl;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Repository;
+
+import com.reader.common.dao.BaseDao;
+import com.reader.core.dao.TestDao;
+import com.reader.core.model.Test;
+
+@Repository("testDAO")
+public class TestDaoImpl extends BaseDao implements TestDao {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"resources/../spring.xml");
+		TestDao testDAOImpl = (TestDao) context.getBean("testDAO");
+
+		List<Test> lt = testDAOImpl.select();
+		for (Test t : lt) {
+			System.out.println(t.getName());
+		}
+
+	}
+
+	public List<Test> select() {
+
+		return getSqlMapClientTemplate().queryForList("selectAllTest", null);
+	}
+
+}
