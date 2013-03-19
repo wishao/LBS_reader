95dbe84468f880bf8666414b13e15a467cea1ecb
wishao <ws_wishao@163.com>
2013-03-19 16:14:32 +0800

    download book test

---
+++ src/test/java/com/test/api/DownLoadTest.java
@@ -0,0 +1,39 @@
+package com.test.api;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class DownLoadTest {
+
+	/**
+	 * @param args
+	 * @throws MalformedURLException
+	 */
+	public static void main(String[] args) throws MalformedURLException {
+		String url = "http://www.qidian.com";
+		String content = "";
+		HttpURLConnection conn = null;
+		try {
+			conn = (HttpURLConnection) (new URL(url).openConnection());
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					conn.getInputStream(), "UTF-8"));
+			String temp = null;
+			StringBuffer buffer = new StringBuffer();
+			while ((temp = reader.readLine()) != null) {
+				buffer.append(temp);
+				//System.out.println(buffer.toString());
+			}
+			content = buffer.toString();
+			System.out.println(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (null != conn)
+				conn.disconnect();
+		}
+	}
+}
